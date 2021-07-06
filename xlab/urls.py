fc3f034d3c018395d2a136cc04630a04ac6acbcf
Mirnaghi Aghazada <mirnaghi7175@gmail.com>
2021-07-06 15:58:21 +0400

    Update urls.py
    change url of blog app

--- xlab/urls.py
+++ xlab/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('/', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
